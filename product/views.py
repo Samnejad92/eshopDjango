e7cdd4032cb1d2758eca49c5666568beb186d646
samnejad92 <m.samnejad92@gmail.com>
2024-07-08 21:08:43 +0330

    Ready to add layout

--- product/views.py
+++ product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest,HttpResponse
 # Create your views here.
+
+def products_list_view(request: HttpRequest):
+    # print(HttpResponse= render(request,'products.html'))
+    return render(request,'products/products.htm')
+
+def products_detail_view(request: HttpRequest):
+    return render(request,'products/details.html')
